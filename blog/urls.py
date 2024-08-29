f41e0a4930410cd20f1103ad69c83079566bfcea
Rachana Mahesh <rachanamahesh9@gmail.com>
2024-08-29 22:14:47 +0530

    created templates and added bootstrap: css and js

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="blog-home"),
+    # Best practice is to keep unique name bcoz anytime we reference that we will be directed to views.home
     path('about/', views.about, name="blog-about"),
 ]
