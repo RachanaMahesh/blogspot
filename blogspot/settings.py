bb86d11d60b6319b36fc0457da7eb28bca222053
Rachana Mahesh <rachanamahesh9@gmail.com>
2024-10-02 00:11:20 +0530

    Part-7 & 8 User Profile Create and Update

--- blogspot/settings.py
+++ blogspot/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -124,6 +125,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+MEDIA_ROOT = os.path.join(BASE_DIR,'media') #it will be in project base directory
+MEDIA_URL = '/media/' # is how we access media dir in browser
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField() #by default required = True jence haven't addes
@@ -8,3 +9,16 @@ class UserRegisterForm(UserCreationForm):
     class Meta: # gives us nested namespace for configuration and kepps the configuration in one space
         model = User # becoz whenever data is validated its going to create user so model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField() #by default required = True jence haven't addes
+
+    class Meta: # gives us nested namespace for configuration and kepps the configuration in one space
+        model = User # becoz whenever data is validated its going to create user so model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta: 
+        model = Profile 
+        fields = ['image']
