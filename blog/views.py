701893c21ed7a195c8aaf6ae07a10e5bb6b31dfb
Rachana Mahesh <rachanamahesh9@gmail.com>
2024-12-15 14:32:59 +0530

    Paginator Part 11

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import post
+from django.contrib.auth.models import User
 from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
@@ -35,6 +36,7 @@ class PostListView(ListView):
     template_name='blog/home.html'
     context_object_name = 'posts'
     ordering = '-date_posted'
+    paginate_by = 5
 
 class PostDetailView(DetailView):
     model = post
@@ -70,4 +72,14 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         if post.author == self.request.user:
             return True
         return False
-    
+
+class UserPostListView(ListView):
+    model = post
+    template_name='blog/user_posts.html'
+    context_object_name = 'posts'
+    # ordering = '-date_posted'
+    paginate_by = 5
+
+    def get_queryset(self,):
+        user = get_object_or_404(User, username= self.kwargs.get('username'))
+        return post.objects.filter(author=user).order_by('-date_posted')
